3e8a06e81164befe74474510401b8d917cb4cbcb
kraleppa <misiek.nalepa@wp.pl>
2020-03-16 20:53:01 +0100

    onp finished

---
+++ src/onp.erl
@@ -0,0 +1,33 @@
+%%%-------------------------------------------------------------------
+%%% @author krzysztof
+%%% @copyright (C) 2020, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 16. mar 2020 20:04
+%%%-------------------------------------------------------------------
+-module(onp).
+-author("krzysztof").
+
+%% API
+-export([calculator/1, parse/1]).
+
+parse(String) ->
+  case string:to_float(String) of
+    {error, no_float} -> list_to_integer(String) * 1.0;
+    {X, _} -> X
+  end.
+
+calculator(Formula) -> calculate(string:tokens(Formula, " "), []).
+
+calculate([], [H | []]) -> H;
+calculate(["+" | T1], [X, Y | T2]) -> calculate(T1, [X + Y | T2]);
+calculate(["-" | T1], [X, Y | T2]) -> calculate(T1, [Y - X | T2]);
+calculate(["*" | T1], [X, Y | T2]) -> calculate(T1, [X * Y | T2]);
+calculate(["/" | T1], [X, Y | T2]) -> calculate(T1, [Y / X | T2]);
+calculate(["sqrt" | T1], [X | T2]) -> calculate(T1, [math:sqrt(X) | T2]);
+calculate(["pow" | T1], [X, Y | T2]) -> calculate(T1, [math:pow(Y, X) | T2]);
+calculate(["sin" | T1], [X | T2]) -> calculate(T1, [math:sin(X) | T2]);
+calculate(["cos" | T1], [X | T2]) -> calculate(T1, [math:cos(X) | T2]);
+calculate([H | T1], Stack) -> calculate(T1, [parse(H) | Stack]).
+
